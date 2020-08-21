a31e4b2a1dc6eb668164fff7ee15ee0c10c39ef9
Matias Olivera <matibjolivera@gmail.com>
2020-08-21 15:08:25 -0300

    log post id

--- payment_notification.php
+++ payment_notification.php
@@ -12,5 +12,6 @@ $log->warning("LOG TEST");
 $log->warning(json_encode($_POST));
 $log->warning(json_encode($_GET));
 $log->warning(json_encode($_REQUEST));
+$log->warning(json_encode($_POST["id"]));
 
 http_response_code(200);
