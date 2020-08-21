f6dfab32a546e371fb9c3767ce340d1ac883da72
Matias Olivera <matibjolivera@gmail.com>
2020-08-21 15:24:19 -0300

    delete http response and logs

--- payment_notification.php
+++ payment_notification.php
@@ -1,13 +1,3 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
-
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-
-$log = new Logger('payment_notification');
-$log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
-
 var_dump($_POST);die();
-
-http_response_code(200);
