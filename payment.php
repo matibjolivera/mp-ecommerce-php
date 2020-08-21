5cfe45b4a4aaad411ea08595dddcb6435cc304de
Matias Olivera <matibjolivera@gmail.com>
2020-08-21 13:36:58 -0300

    use test accounts

--- payment.php
+++ payment.php
@@ -8,7 +8,7 @@ if (!$_POST || !$_POST['product_title'] || !$_POST['product_price'] || !$_POST['
 /**
  * Sandbox token
  */
-MercadoPago\SDK::setAccessToken('TEST-8948539626952155-061321-a29ce4bf0c00578dba394ef90d91c230-261498730');
+MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
 $baseSiteUrl = "https://{$_SERVER["HTTP_HOST"]}";
 
@@ -64,4 +64,4 @@ $preference->payer = $payer;
 
 $preference->save();
 
-header("Location: {$preference->sandbox_init_point}");
+header("Location: {$preference->init_point}");
