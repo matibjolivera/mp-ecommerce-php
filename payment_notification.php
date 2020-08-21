07e87b29158acb66dbc3ec64c6f0325f368f2f69
Matias Olivera <matibjolivera@gmail.com>
2020-08-21 14:39:32 -0300

    change addWarning by warning

--- payment_notification.php
+++ payment_notification.php
@@ -8,8 +8,7 @@ use Monolog\Handler\StreamHandler;
 $log = new Logger('payment_notification');
 $log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
 
-$log->addWarning("LOG TEST");
-$log->addWarning(json_encode($_POST));
-
+$log->warning("LOG TEST");
+$log->warning(json_encode($_POST));
 
 http_response_code(200);
