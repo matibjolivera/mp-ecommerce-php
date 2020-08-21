b31c81970c37241fa154f3146a8a76bca27876c8
Matias Olivera <matibjolivera@gmail.com>
2020-08-21 15:28:20 -0300

    use file get contents instead

--- payment_notification.php
+++ payment_notification.php
@@ -1,3 +1,14 @@
 <?php
 
-echo file_get_contents('php://input');die();
+require __DIR__ . '/vendor/autoload.php';
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$log = new Logger('payment_notification');
+$log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
+
+$post = file_get_contents('php://input');
+$log->warning("POST: " . $post);
+
+http_response_code(200);
