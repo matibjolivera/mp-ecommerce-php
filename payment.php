8511ac8ee04840b2dc04892148241cbb4419d24f
Matias Olivera <matibjolivera@gmail.com>
2020-08-21 13:59:13 -0300

    delete line

--- payment.php
+++ payment.php
@@ -5,10 +5,9 @@ require __DIR__ . '/vendor/autoload.php';
 if (!$_POST || !$_POST['product_title'] || !$_POST['product_price'] || !$_POST['product_image']) {
     header("Location: index.php");
 }
-https://matibjolivera-mp-commerce-php.herokuapp.com/success?collection_id=9236956216&collection_status=approved&external_reference=null&payment_type=credit_card&merchant_order_id=1701372043&preference_id=469485398-1761af2f-924c-42de-99a7-1ff9d6b5da5e&site_id=MLA&processing_mode=aggregator&merchant_account_id=null
+
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
-
 
 $baseSiteUrl = "https://{$_SERVER["HTTP_HOST"]}";
 
