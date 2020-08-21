efac2d9ec5a20e05e7a700cf34d124dbfc92b1b5
Matias Olivera <matibjolivera@gmail.com>
2020-08-21 01:20:24 -0300

    fix image uri in item data

--- payment.php
+++ payment.php
@@ -5,7 +5,6 @@ require __DIR__ . '/vendor/autoload.php';
 if (!$_POST || !$_POST['product_title'] || !$_POST['product_price'] || !$_POST['product_image']) {
     header("Location: index.php");
 }
-var_dump("{$_SERVER["HTTP_HOST"]}/{$_POST['product_image']}");die();
 /**
  * Sandbox token
  */
@@ -16,7 +15,7 @@ $preference = new MercadoPago\Preference();
 $item = new MercadoPago\Item();
 $item->id = 1234;
 $item->description = "Dispositivo móvil de Tienda e-commerce";
-$item->picture_url = "{$_SERVER["HTTP_HOST"]}{$_SERVER["REQUEST_URI"]}{$_POST['product_image']}";
+$item->picture_url = "https://{$_SERVER["HTTP_HOST"]}/{$_POST['product_image']}";
 $item->title = $_POST['product_title'];
 $item->quantity = 1;
 $item->unit_price = $_POST['product_price'];
