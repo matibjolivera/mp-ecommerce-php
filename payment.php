9a73688f0ef7a63402e075118a96d4e6cfbbda99
Matias Olivera <matibjolivera@gmail.com>
2020-08-21 13:45:44 -0300

    integrator id

--- payment.php
+++ payment.php
@@ -5,10 +5,10 @@ require __DIR__ . '/vendor/autoload.php';
 if (!$_POST || !$_POST['product_title'] || !$_POST['product_price'] || !$_POST['product_image']) {
     header("Location: index.php");
 }
-/**
- * Sandbox token
- */
+
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
+
 
 $baseSiteUrl = "https://{$_SERVER["HTTP_HOST"]}";
 
