f89f1bc5a69425396f301a22718037788c447534
Matias Olivera <matibjolivera@gmail.com>
2020-08-21 14:27:43 -0300

    monolog

--- payment_notification.php
+++ payment_notification.php
@@ -1,5 +1,13 @@
 <?php
 
-file_put_contents("php://stderr", json_encode($_POST) . "\n");
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$log = new Logger('payment_notification');
+$log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
+
+$log->addWarning("LOG TEST");
+$log->addWarning(json_encode($_POST));
+
 
 http_response_code(200);
